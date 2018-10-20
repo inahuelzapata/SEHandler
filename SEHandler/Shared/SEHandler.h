4a7f25257ae8e91a312d47d99d99a1d4e3aeee9c
Nahuel Zapata <inahuelzapata@gmail.com>
2018-10-20 18:04:51 -0300

    Initial commit.

---
+++ SEHandler/Shared/SEHandler.h
@@ -0,0 +1,19 @@
+//
+//  SEHandler.h
+//  SEHandler
+//
+//  Created by Nahuel Zapata on 10/20/18.
+//  Copyright © 2018 iNahuelZapata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SEHandler.
+FOUNDATION_EXPORT double SEHandlerVersionNumber;
+
+//! Project version string for SEHandler.
+FOUNDATION_EXPORT const unsigned char SEHandlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SEHandler/PublicHeader.h>
+
+
